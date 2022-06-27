173755839e521fdb368c48558aeb1bf5061b2cd0
Weronika Materkowska <w.m.materkowska@gmail.com>
2022-06-28 01:34:32 +0200

    initial tests

---
+++ src/test/java/org/example/CsvProcessorTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CsvProcessorTest {
+
+    @Test
+    void getHeadersFromCsv() {
+    }
+
+    @Test
+    void numberOfHeaders() {
+    }
+}
