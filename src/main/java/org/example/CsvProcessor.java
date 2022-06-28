95b64ec6705d8d2fbd50759301fe632660e4c7e5
Weronika Materkowska <w.m.materkowska@gmail.com>
2022-06-29 01:03:07 +0200

    CSV binding with Brewery entity using CSV Processor initial functionality in the Analyzer

--- src/main/java/org/example/CsvProcessor.java
+++ src/main/java/org/example/CsvProcessor.java
@@ -2,31 +2,93 @@ package org.example;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
+import com.opencsv.exceptions.CsvMalformedLineException;
+import com.opencsv.exceptions.CsvValidationException;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CsvProcessor {
 
-    String fileName;
-    ArrayList<String> headers = new ArrayList<>();
+    // retrieve the headers from csv file
+    public ArrayList<String> getHeadersFromCsv(String filename) throws IOException, CsvException {
+        ArrayList<String> headers = new ArrayList<>();
 
-    public ArrayList<String> getHeadersFromCsv() throws IOException, CsvException {
-        fileName = "src/main/resources/breweries_usa.csv";
-        try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+        try (CSVReader reader = new CSVReader(new FileReader(filename))) {
             String[] header = reader.readNext();
             for (int fieldNr = 0; fieldNr < header.length; fieldNr++) {
                 headers.add(header[fieldNr]);
             }
-            System.out.println(headers);
         }
         return headers;
     }
 
-    public int numberOfHeaders(){
-
-        return headers.size();
+    // return number of headers
+    public int numberOfHeaders(String filename) throws IOException, CsvException {
+        ArrayList<String> headers = getHeadersFromCsv(filename);
+        int numberOfHeaders = headers.size();
+        return numberOfHeaders;
+        //return headers.size();
     }
+
+
+    // read Csv file and print it to the terminal
+    public void readCsvAndPrint(String filename){
+        try (CSVReader reader = new CSVReader(new FileReader(filename))) {
+            List<String[]> r = reader.readAll();
+            reader.close();
+            r.forEach(x -> System.out.println(Arrays.toString(x)));
+        } catch (CsvMalformedLineException | FileNotFoundException e){
+            System.out.println("invalid data " + e);
+        } catch (IOException | CsvException e) {
+            System.out.println("invalid data " + e);
+            //throw new RuntimeException(e);
+        }
+    }
+
+    // read Csv file and put it to the list
+    public List<String[]> readCsvToList(String filename){
+            List<String[]> r = new ArrayList<>();
+        try (CSVReader reader = new CSVReader(new FileReader(filename))) {
+            r = reader.readAll();
+            reader.close();
+            r.forEach(x -> System.out.println(Arrays.toString(x)));
+
+        } catch (CsvMalformedLineException | FileNotFoundException e){
+            System.out.println("invalid data " + e);
+        } catch (IOException | CsvException e) {
+            System.out.println("invalid data " + e);
+            //throw new RuntimeException(e);
+        }
+        return r;
+    }
+
+    // read file line by line and print it to terminal
+    public void readCsvLineByLineAndPrint(String filename){
+        // read file line by line and print it to terminal
+        try (CSVReader reader = new CSVReader(new FileReader(filename))) {
+
+            String[] lineInArray;
+            while ((lineInArray = reader.readNext()) != null) {
+                for(int column = 0 ; column < lineInArray.length; column++ ){
+                    System.out.print(lineInArray[column] + "|");
+                }
+                System.out.println();
+            }
+        } catch(CsvMalformedLineException e) {
+            System.out.println("invalid data" + e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException | CsvValidationException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
 
 }
---
+++ src/test/java/org/example/BreweryAnalyzerTest.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BreweryAnalyzerTest {
+
+    @Test
+    void numberOfBreweriesInEachStateMap() {
+
+    }
+
+    @Test
+    void topCitiesForBreweries() {
+    }
+
+    @Test
+    void numberOfBreweryWithWebsite() {
+        // arrange
+
+        // act
+        // verify
+    }
+
+    @Test
+    void numberOfBreweryInDelawareWithTacos() {
+    }
+
+    @Test
+    void breweriesOffersWinePercent() {
+    }
+}
